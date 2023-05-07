e42302e6414dfc2156decf4d1dd80e789453e342
ParkIsComing <maroony55@gmail.com>
2023-05-08 03:41:42 +0900

    feat : 초기 세팅

---
+++ src/main/java/efub/toy/mangoplate/MangoplateApplication.java
@@ -0,0 +1,13 @@
+package efub.toy.mangoplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangoplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MangoplateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/efub/toy/mangoplate/MangoplateApplicationTests.java
@@ -0,0 +1,13 @@
+package efub.toy.mangoplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MangoplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
